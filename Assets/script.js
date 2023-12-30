d09cbb06d814fa1e997b47a1d0be24c2d880789f
Luis Moore <luismoore@Luiss-MacBook-Air.local>
2023-12-30 15:32:56 +0000

    added global variables

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+var cityInputEl = document.querySelector("#city-input");
+var searchForm = document.querySelector("#search-form");
+var clearBtn = document.querySelector("#clear-history-button");
+var historySearch = document.querySelector("#search-history");
+var todayWeather = document.querySelector("#today-weather");
+var fiveForecast = document.querySelector("#fiveForecast");
+var searchHistory = [];
